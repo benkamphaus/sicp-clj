87a446adec7cb20915ed7d094f5872d12649bf8d
Ben Kamphaus <ben.kamphaus@gmail.com>
2014-08-24 10:28:57 -0600

    Removing cruft.

--- src/sicp_clj/newton.clj
+++ src/sicp_clj/newton.clj
@@ -6,9 +6,6 @@
 (defn square [x]
   (* x x))
 
-(defn cube [x]
-  (* x x))
-
 (defn average [x y]
   (/ (+ x y) 2))
 
@@ -22,14 +19,3 @@
         guess
         (sqrt-iter (improve guess))))
   (sqrt-iter 1.0))
-
-(defn cube-root [x]
-  (defn good-enough? [guess]
-    (< (abs (- (cube guess) x)) 0.001))
-  (defn improve [guess]
-    (/ (+ (* 2 guess) (/ x (square guess))) 3.0))
-  (defn cube-rt-iter [guess]
-    (if (good-enough? guess)
-        guess
-        (cube-rt-iter (improve guess))))
-  (cube-rt-iter 1.0))
