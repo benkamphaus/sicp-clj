0222c676c7f5d00f9c0d51fb3d637911d40af471
Ben Kamphaus <ben.kamphaus@gmail.com>
2014-08-24 12:38:22 -0600

    First class functioning.

--- src/sicp_clj/newton.clj
+++ src/sicp_clj/newton.clj
@@ -1,21 +1,41 @@
 (ns sicp-clj.newton)
 
-(defn abs [x]
-  (if (>= x 0) x (- x)))
-
 (defn square [x]
   (* x x))
 
-(defn average [x y]
-  (/ (+ x y) 2))
+(defn abs [a]
+  (if (< a 0) (- a) a))
+
+(defn fixed-point [f start]
+  (def tolerance 0.00001)
+  (defn close-enough? [u v]
+    (< (abs (- u v)) tolerance))
+  (defn iter [old new]
+    (if (close-enough? old new)
+      new
+      (iter new (f new))))
+  (iter start (f start)))
+
+(defn deriv [f]
+  (let [dx 0.00001]
+    (fn [x]
+      (/ (- (f (+ x dx))
+            (f x))
+         dx))))
+
+(defn newton [f guess]
+  (let [df (deriv f)]
+    (fixed-point
+      #(- % (/ (f %) (df %)))
+      guess)))
 
 (defn sqrt [x]
-  (defn good-enough? [guess]
-    (< (abs (- (square guess) x)) (/ 0.0001 x)))
-  (defn improve [guess]
-    (average guess (/ x guess)))
-  (defn sqrt-iter [guess]
-    (if (good-enough? guess)
-        guess
-        (sqrt-iter (improve guess))))
-  (sqrt-iter 1.0))
+  (newton #( - x (square %))
+          1.0))
+
+(defn cube [x]
+  (* x x x))
+
+(defn cube-root [x]
+  (newton #(- x (cube %))
+          1.0))
