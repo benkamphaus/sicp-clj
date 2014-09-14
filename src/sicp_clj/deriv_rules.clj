fdd3faa354de77171308c9238a3c5a601c2f5b49
Ben Kamphaus <ben.kamphaus@gmail.com>
2014-09-14 11:45:26 -0600

    Incrementally building it out.

--- src/sicp_clj/deriv_rules.clj
+++ src/sicp_clj/deriv_rules.clj
@@ -1,6 +1,26 @@
 (ns sicp-clj.deriv-rules)
+;; Not running, needs first/rest car/cdr/cadr equivs in let statement
+;; before make-sum, make-product, etc.
 
-(defn deriv [exp vars]
+(defn atom? [x]
+  (= 1 (count x)))
+
+(defn constant? [exp var]
+  (and (atom? exp)
+       (not (= exp var))))
+
+(defn same-var? [exp var]
+  (and (atom? exp)
+       (= exp var)))
+
+(defn sum? [exp]
+  (and (not (atom? exp))
+       (= (first exp) (quote +))))
+
+(defn make-sum [a1 a2]
+  (list (quote +) a1 a2))
+
+(defn deriv [exp var]
   (cond (constant? exp var) 0
         (same-var? exp var) 1
         (sum? exp)
